e0a151c4fb572c3c38d30226719fdf8ac5ecb161
Limado <noliemiliano@gmail.com>
2022-09-02 12:06:18 -0300

    primer subida a produccion real

---
+++ public/assets/js/resources/ENG.js
@@ -0,0 +1,169 @@
+'use strict';
+const Resources = [{
+        "key": "about",
+        "value": "ABOUT"
+    },
+    {
+        "key": "about_description_1_line_1",
+        "value": "Enthusiastic and pasionate by software and web development."
+    },
+    {
+        "key": "about_description_1_line_2",
+        "value": "Always looking to be better, researching, training myself, learning new technics and programming stacks."
+    },
+    {
+        "key": "about_description_1_line_3",
+        "value": "Proud father of 2 beautiful son and married to a wonderful woman."
+    },
+    {
+        "key": "about_description_1_line_4",
+        "value": "Motorbike lover and ciclism fanatic."
+    },
+    {
+        "key": "about_description_2",
+        "value": "10+ years experience enjoying programming."
+    },
+    {
+        "key": "about_description_3",
+        "value": "In continuous training, either independently or with the help of professionals, always trying to be up to date with technologies, in love with JavaScript since years, both, backend and frontend."
+    },
+    {
+        "key": "birthday",
+        "value": "Birthday:"
+    },
+    {
+        "key": "birthdate",
+        "value": "February 20, 1985"
+    },
+    {
+        "key": "phone",
+        "value": "Phone:"
+    },
+    {
+        "key": "city",
+        "value": "City:"
+    },
+    {
+        "key": "age",
+        "value": "Age:"
+    },
+    {
+        "key": "email",
+        "value": "Email:"
+    },
+    {
+        "key": "facts_1",
+        "value": "Despite the fact that my career, for the most part, has been in a dependency relationship, that has not prevented me from carrying out Freelance projects, in my free time, with the intention of adding experience and knowledge."
+    },
+    {
+        "key": "frontend",
+        "value": "Web projects"
+    },
+    {
+        "key": "repositories",
+        "value": "Github repositories"
+    },
+    {
+        "key": "backend",
+        "value": "Other projects"
+    },
+    {
+        "key": "skills",
+        "value": "Skills"
+    },
+    {
+        "key": "skillsSummary_1",
+        "values": "En mi constante búsqueda de apredizaje he pasado por varios stack, incluyendo algunos de diseño web, que no he dejado de utilizar durante mi trayectoria.",
+        "value": "In a constant search of knowledge, I've learned severals stacks (including some web design ones) that I've never stopped using."
+    },
+    {
+        "key": "skillsSummary_2",
+        "value": "These are some of them and my years of experience:"
+    },
+    {
+        "key": "resume",
+        "value": "Resume"
+    },
+    {
+        "key": "integration",
+        "value": "Integrations"
+    },
+    {
+        "key": "resumeContent",
+        "value": ""
+    },
+    {
+        "key": "sumary",
+        "value": "Sumary"
+    },
+    {
+        "key": "myname",
+        "value": "Emiliano Noli"
+    },
+    {
+        "key": "mynamesumary",
+        "value": "Innovative and deadline-driven fullstack developer with 10+ years of experience developing B2B and B2C  web software, and 1 years of experience as develpment team leader"
+    },
+    {
+        "key": "organics_position",
+        "value": "Development team Leader"
+    },
+    {
+        "key": "organics_li_1",
+        "value": "Lead and develop software projects to our clients"
+    },
+    {
+        "key": "organics_li_2",
+        "value": "Development consulting"
+    },
+    {
+        "key": "neotel_position",
+        "value": "Fullstack developer"
+    },
+    {
+        "key": "neotel_li_1",
+        "value": "Develop new features and maintenance company main product"
+    },
+    {
+        "key": "neotel_li_2",
+        "value": "Lead and develop new products from scratch"
+    },
+    {
+        "key": "neotel_li_3",
+        "value": "Lead and develop integrations with external platforms"
+    },
+    {
+        "key": "neotel_li_4",
+        "value": "ISO-9001 quality process manager"
+    },
+    {
+        "key": "reportv_position",
+        "value": "Web developer leader"
+    },
+    {
+        "key": "reportv_li_1",
+        "value": "Web development leaders"
+    },
+    {
+        "key": "reportv_li_2",
+        "value": "Server administrator (Ubuntu/mysql/apache2)"
+    },
+    {
+        "key": "opratel_position",
+        "value": "Junior developer"
+    },
+    {
+        "key": "opratel_p_1",
+        "value": "My very first oficial job as developer"
+    },
+    {
+        "key": "opratel_p_2",
+        "value": "Mobile wap portals still working those days, sms service subcriptions too"
+    },
+    {
+        "key": "ape_sumary",
+        "value": "Co-founder at APE Developers startup. A software development company"
+    }
+]
+
+export default { languaje: 'SPA', resources: Resources };
---
+++ public/assets/js/resources/SPA.js
@@ -0,0 +1,167 @@
+'use strict';
+const Resources = [{
+        "key": "about",
+        "value": "Sobre mi"
+    },
+    {
+        "key": "about_description_1_line_1",
+        "value": "Entusiasta y apasionado por el desarrollo de software."
+    },
+    {
+        "key": "about_description_1_line_2",
+        "value": "Buscando siempre la manera mejorar, investigando, formandome, aprendiendo nuevas técnicas y lenguajes."
+    },
+    {
+        "key": "about_description_1_line_3",
+        "value": "Padre orgulloso de 2 hermosos hijos y esposo de una mujer maravillosa."
+    },
+    {
+        "key": "about_description_1_line_4",
+        "value": "Amante de las motos y fanatico del ciclismo."
+    },
+    {
+        "key": "about_description_2",
+        "value": "Mas de 10 años de experiencia disfrutando programar."
+    },
+    {
+        "key": "about_description_3",
+        "value": "En formacion continua, ya sea de manera independiente ó con ayuda de profesionales, intento estar siempre al día con las tecnologias, inclinadome hace unos años al mundo JavaScript, tanto backend como frontend."
+    },
+    {
+        "key": "birthday",
+        "value": "Fecha de nacimiento:"
+    },
+    {
+        "key": "birthdate",
+        "value": "20 Febrero 1985"
+    },
+    {
+        "key": "phone",
+        "value": "Móvil:"
+    },
+    {
+        "key": "city",
+        "value": "Ciudad:"
+    },
+    {
+        "key": "age",
+        "value": "Edad:"
+    },
+    {
+        "key": "email",
+        "value": "Email:"
+    },
+    {
+        "key": "facts_1",
+        "value": "A pesar de que mi carrera, en su mayoría, ha sido en relación de dependencia, eso no me ha impedido realizar proyectos Freelance, en mis tiempos libres, con la intecion de sumar experiencia y conocimientos."
+    },
+    {
+        "key": "frontend",
+        "value": "Proyectos web"
+    },
+    {
+        "key": "repositories",
+        "value": "Repositorios en github"
+    },
+    {
+        "key": "backend",
+        "value": "Otros proyectos"
+    },
+    {
+        "key": "skills",
+        "value": "Conocimientos"
+    },
+    {
+        "key": "skillsSummary_1",
+        "value": "En mi constante búsqueda de apredizaje he pasado por varios stacks (incluyendo algo de diseño web) que no he dejado de utilizar durante mi trayectoria."
+    },
+    {
+        "key": "skillsSummary_2",
+        "value": "Estos son algunos de ellos y los años que llevo usandolos:"
+    },
+    {
+        "key": "resume",
+        "value": "Trayectoria"
+    },
+    {
+        "key": "resumeContent",
+        "value": ""
+    },
+    {
+        "key": "integration",
+        "value": "Integrations"
+    },
+    {
+        "key": "sumary",
+        "value": "Resumen"
+    },
+    {
+        "key": "myname",
+        "value": "Emiliano Noli"
+    },
+    {
+        "key": "mynamesumary",
+        "value": "Desarrollador fullstack con más de 10 años de experiencia en el desarrollo de software web B2B y B2C, y 1 año de experiencia como líder del equipo de desarrollo."
+    },
+    {
+        "key": "organics_position",
+        "value": "Team leader"
+    },
+    {
+        "key": "organics_li_1",
+        "value": "Liderazgo y desarrollo de software para nuestros clientes"
+    },
+    {
+        "key": "organics_li_2",
+        "value": "Consultoría en desarrollo"
+    },
+    {
+        "key": "neotel_position",
+        "value": "Fullstack developer"
+    },
+    {
+        "key": "neotel_li_1",
+        "value": "Desarrollo y mantenimiento del producto principal de la empresa"
+    },
+    {
+        "key": "neotel_li_2",
+        "value": "Desarrollo de nuevos productos"
+    },
+    {
+        "key": "neotel_li_3",
+        "value": "Desarrollo de integraciones con plataformas externas"
+    }, {
+        "key": "neotel_li_4",
+        "value": "Responsable de procesos de calidad ISO-9001"
+    },
+    {
+        "key": "reportv_position",
+        "value": "Lider en desarrollos web"
+    },
+    {
+        "key": "reportv_li_1",
+        "value": "Lider en desarrollos web"
+    },
+    {
+        "key": "reportv_li_2",
+        "value": "Administradore de servidores web (Ubuntu/mysql/apache2)"
+    },
+    {
+        "key": "opratel_position",
+        "value": "Desarrollador junior"
+    },
+    {
+        "key": "opratel_p_1",
+        "value": "Mi primer empleo como desarrollador"
+    },
+    {
+        "key": "opratel_p_2",
+        "value": "Desarrollo de portales wap mobile para servicios de suscripcion sms"
+    },
+    {
+        "key": "ape_sumary",
+        "value": "Co-fundador de la startup APE Developers. Empresa dedicada al desarrollo de software a medida."
+    }
+]
+
+export default { languaje: 'SPA', resources: Resources };
